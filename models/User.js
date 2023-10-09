f068e389d9e1965befa3a5c7cfd7a465a7e480cc
Ritesh Maurya <riteshkumar411552@gmail.com>
2023-10-09 13:18:54 +0530

    added custom validaton in user schema

--- models/User.js
+++ models/User.js
@@ -3,16 +3,24 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     name:{
         type:String,
-        required:true
+        required:true,
+        maxLength:[25, "Name can't be greater than 25 characters"]
     },
     email:{
         type:String,
         required:true,
-        unique:true
+        unique:true,
+        match: [/.+\@.+\../, "Please enter a valid email"]
     },
     password:{
         type:String,
-        required:true
+        required:true,
+        validate:{
+            validator:function(value) {
+                return /^(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,12}$/.test(value)
+            },
+            message:"Password should be between 8-12 charachetrs and have a special character"
+        }
     }
 
 });
