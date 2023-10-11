04f03f4b9aa5a00aa67c212af86b37589b314224
Ritesh Maurya <riteshkumar411552@gmail.com>
2023-10-11 11:53:44 +0530

    implemented controller for getSingalComment by id

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -2,9 +2,9 @@ const Comment = require('../models/Comment');
 const Post = require('../models/Post');
 
 const createComment = async (req, res) => {
-    console.log(req.user);
+    const { content, post } = req.body;
     try {
-        const { content, post } = req.body;
+
         const posts = await Post.findById(post);
         if (posts) {
             const comment = await Comment.create({
@@ -12,7 +12,7 @@ const createComment = async (req, res) => {
                 user: req.user._id,
                 post: post
             });
-            
+
             posts.comments.push(comment);
             await posts.save();
             res.status(201).json({ msg: 'Comment created' });
@@ -27,18 +27,24 @@ const createComment = async (req, res) => {
 
 
 
-const getAllComments = async(req, res) => {
-  try {
-     const comments = await Comment.find({});
-     res.status(200).json({comments});
-  } catch (error) {
-    console.log(error);
-  }
+const getAllComments = async (req, res) => {
+    try {
+        const comments = await Comment.find({});
+        res.status(200).json({ comments });
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 
-const getSingalComment = (req, res) => {
-
+const getSingalComment = async(req, res) => {
+    const {id} = req.params;
+    try {
+       const comment = await Comment.findOne({post:id});
+       res.status(200).json({comment});
+    } catch (error) {
+  console.log(error);
+    }
 }
 
 const updateComment = (req, res) => {
