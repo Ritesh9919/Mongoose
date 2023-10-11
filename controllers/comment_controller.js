bb5f9721c298a597f0f421e182f800580eb5906b
Ritesh Maurya <riteshkumar411552@gmail.com>
2023-10-11 11:40:13 +0530

    implemented controller for create comment

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -1,7 +1,28 @@
 const Comment = require('../models/Comment');
+const Post = require('../models/Post');
 
-const createComment = (req, res) => {
-   
+const createComment = async (req, res) => {
+    console.log(req.user);
+    try {
+        const { content, post } = req.body;
+        const posts = await Post.findById(post);
+        if (posts) {
+            const comment = await Comment.create({
+                content: content,
+                user: req.user._id,
+                post: post
+            });
+            
+            posts.comments.push(comment);
+            await posts.save();
+            res.status(201).json({ msg: 'Comment created' });
+
+        }
+
+    } catch (error) {
+        console.log(error);
+    }
+
 }
 
 
